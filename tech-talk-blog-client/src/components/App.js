123528e07478bbbae7a9672455ea6daf106558f5
andisiambuku <roselandambuku@gmail.com>
2022-11-08 12:56:57 +0300

    feature: added the header

--- tech-talk-blog-client/src/components/App.js
+++ tech-talk-blog-client/src/components/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Home from "./Home.js"
-import About from "/About.js"
+import About from "./About.js"
+import Header from "./Header.js"
+import Footer from "./Footer.js"
+import AddNewPost from "./AddNewPost.js"
 import {Route, Routes} from "react-router-dom";
 
 
@@ -10,8 +13,10 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/header" element={< Header />} />
         <Route path="/about" element={< About />} />
-        <Route path="/addnew" element={<AddNew />} /> 
+        <Route path="/addnew" element={<AddNewPost />} /> 
+        <Route path="/footer" element={<Footer />} /> 
         {/* <Route path="articles/:id" element={<Article />} /> */}
 
       </Routes>
