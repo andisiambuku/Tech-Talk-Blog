123bc093ce8b0ed1e734da538ce63c03d611f14b
Roseland Andisi Ambuku <roselandambuku@gmail.com>
2022-11-09 17:16:36 +0300

    feature: fetch hook

---
+++ tech-talk-blog-client/src/hooks/useFetch.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+
+const useFetch=(url)=>{
+   
+    const [data,setData] = useState([]);
+    const [isPending,setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(()=>{
+       
+            fetch(url)
+            .then((res) =>  res.json())
+            .then((data) =>{
+                
+                setData(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch((err)=>{
+                if(err.name==='AbortError'){
+                    console.log('Fetch aborted');
+                }
+                else{
+                    setError(err.message);
+                    setIsPending(false);
+                }
+        })
+        
+    },[url]);
+
+    return { data, isPending, error };
+
+}
+
+export default useFetch;
