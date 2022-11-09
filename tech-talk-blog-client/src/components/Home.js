f4205fbd0473bddf8e782fef15be33c3f644097d
Roseland Andisi Ambuku <roselandambuku@gmail.com>
2022-11-09 19:30:22 +0300

    feature: added home component

--- tech-talk-blog-client/src/components/Home.js
+++ tech-talk-blog-client/src/components/Home.js
@@ -1,9 +1,20 @@
 import React from "react"
+// import ArticleList from "./ArticleList"
+import useFetch from "../hooks/useFetch"
 
 function Home(){
+    // const {data: articles, isPending, error} = useFetch('http://localhost:3001/articles');
+    const {data: isPending,error} = useFetch(url);
     return(
         <div className="Home">
-            <h1>Hello World</h1>
+            { error && <div>{error}</div>}
+            { isPending && <div>Loading...</div>}
+            <div className="home-display">
+                <h2 className="title">
+                    All Posts
+                </h2>
+                {/* <ArticleList articles={articles}/> */}
+            </div>
         </div>
     );
     
