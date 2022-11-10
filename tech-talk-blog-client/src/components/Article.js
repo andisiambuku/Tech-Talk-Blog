0b746e4dae7948a74f49bebc67d5c171bb0cdb51
Roseland Andisi Ambuku <roselandambuku@gmail.com>
2022-11-10 16:05:47 +0300

    feature: created like/dislike button

--- tech-talk-blog-client/src/components/Article.js
+++ tech-talk-blog-client/src/components/Article.js
@@ -1,10 +1,14 @@
-import React  from 'react';
+import React,{ useState}  from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import useFetch from '../hooks/useFetch';
 
 
 
 function Article() {
+    const [likeCount,setLikeCount] = useState(50);
+    const [dislikeCount,setDislikeCount] = useState(50);
+    const [activeBtn, setActiveBtn] = useState("none");
+    
     const { id } = useParams();
    
     const { data: article,error,isPending } = useFetch(`http://localhost:9292/post/${id}`);
@@ -24,6 +28,45 @@ function Article() {
             navigate('/');
         })
     }
+    const handleLikeClick = () => {
+        if (activeBtn === "none") {
+          setLikeCount(likeCount + 1);
+          setActiveBtn("like");
+          return;
+        }
+     
+        if (activeBtn === 'like'){
+          setLikeCount(likeCount - 1);
+          setActiveBtn("none");
+          return;
+        }
+     
+        if (activeBtn === "dislike") {
+          setLikeCount(likeCount + 1);
+          setDislikeCount(dislikeCount - 1);
+          setActiveBtn("like");
+        }
+      };
+      const handleDisikeClick = () => {
+        if (activeBtn === "none") {
+          setDislikeCount(dislikeCount + 1);
+          setActiveBtn("dislike");
+          return;
+        }
+       
+        if (activeBtn === 'dislike'){
+          setDislikeCount(dislikeCount - 1);
+          setActiveBtn("none");
+          return;
+        }
+     
+        if (activeBtn === "like") {
+          setDislikeCount(dislikeCount + 1);
+          setLikeCount(likeCount - 1);
+          setActiveBtn("dislike");
+        }
+      };
+    
     return(
         <div className='article-detail'>
             {isPending && <div>Loading...</div>}
@@ -36,8 +79,28 @@ function Article() {
 
                 {/* <p className='article-author'>Written by { article.author }</p> */}
                 <div className='article-body'>{ article.content }</div>
+
                 
             </article>
+            <div className="container">
+  <div className="btn-container">
+    <button
+      className={`btn ${activeBtn === "like" ? "like-active" : ""}`}
+      onClick={handleLikeClick}
+    >
+      <span className="material-symbols-rounded">thumb_up</span>
+      Like {likeCount}
+    </button>
+ 
+    <button
+      className={`btn ${activeBtn === "dislike" ? "dislike-active" : ""}`}
+      onClick={handleDisikeClick}
+    >
+      <span className="material-symbols-rounded">thumb_down</span>
+      Dislike {dislikeCount}
+    </button>
+  </div>
+</div>;
 
         </div>
     );
